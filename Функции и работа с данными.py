075c403ddd280ec8c275c016d831b324caae3d48
Daria <dariaseverina@mail.ru>
2024-07-09 16:09:29 +0300

    edit 1

---
+++ Функции и работа с данными.py
@@ -0,0 +1,41 @@
+#№1
+
+import pandas as pd
+ratings = pd.read_csv('ratings.csv')
+ratings.head()
+ratings = ratings.groupby('movieId').mean().reset_index()
+ratings.head()
+def classify(rating):
+    if rating <= 2:
+        return 'низкий рейтинг'
+    if 2 < rating <= 4:
+        return 'средний рейтинг'
+    if 4 < rating <= 5:
+        return 'высокий рейтинг'
+ratings['class'] = ratings['rating'].apply(classify)
+print(ratings.head(100))
+
+#№2
+
+import pandas as pd
+keywords = pd.read_csv('keywords.csv')
+keywords.head(10)
+def classify(keyword):
+geo_data = {
+    'Центр': ['москва', 'тула', 'ярославль'],
+    'Северо-Запад': ['петербург', 'псков', 'мурманск'],
+    'Дальний Восток': ['владивосток', 'сахалин', 'хабаровск']
+}
+new_geo_data = {}
+for i in geo_data:
+    for j in geo_data[i]:
+        new_geo_data[j] = i
+def define_reg(i):
+    for j in new_geo_data.keys():
+        if j.lower() in i.lower():
+            return j
+            break
+    return 'undefined'
+keywords['region'] = keywords['keyword'].apply(define_reg)
+print(keywords.head(10))
+
